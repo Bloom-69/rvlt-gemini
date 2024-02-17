ea4a8e6c005c3b95c33b6ed86d9c3b794ab22e55
bloom <val.perez@cemep.edu.do>
2024-02-16 20:42:34 -0400

    fix: fix a "small" issue which I forgot

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const client = new Client();
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_TOKEN);
 const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
 const generationConfig = {
   temperature: 1,
