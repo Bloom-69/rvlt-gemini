37c855907229742ea29223232f74398328e859d0
bloom <val.perez@cemep.edu.do>
2024-02-18 12:57:02 -0400

    feat: colorized logging

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import colors from "colors"
 import {
   GoogleGenerativeAI,
   HarmCategory,
@@ -11,7 +12,7 @@ dotenv.config();
 import fetch from "node-fetch";
 
 const client = new Client();
-if (!process.env.GEMINI_API_TOKEN, process.env.GEMINI_API_TOKEN === "") throw new Error("[ERROR] Missing Gemini API token");
+if (!process.env.GEMINI_API_TOKEN, process.env.GEMINI_API_TOKEN === "") throw new Error(colors.bgRed("ERROR"), "Missing Gemini API token");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_TOKEN);
 const text_model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
 const image_model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
@@ -62,59 +63,60 @@ function fileToGenerativePart(buffer, mimeType) {
 }
 
 client.on("ready", () => {
-  console.log(`[READY] Logged in as ${client.user?.username}!`);
+  console.log(colors.bgGreen.white("READY"), "Logged in as", colors.blue(client.user?.username));
 });
 
 client.on("messageCreate", async (msg) => {
   try {
     if (msg.author?.bot) {
-      console.log("[MESSAGE] Message is from a bot, ignoring...");
+      console.log(colors.bgYellow("WARN"), "Message is from a bot, ignoring...");
       return 0;
     } else {
       if (msg.attachments) {
         const imageParts = [];
-        console.log("[GEMINI_API] Using: ", image_model.model.toString());
-        console.log("[MESSAGE] This message has attachments: ", msg.attachments);
+        console.log(colors.bgMagenta("GEMINI_API"), "Using: ", image_model.model.toString());
+        console.log(colors.dim("MESSAGE"), "This message has attachments: ", msg.attachments);
         for (const attachment of msg.attachments) {
-          console.log("[ATTACHMENT] Downloading attachment: ", attachment.url);
+          console.log(colors.bgWhite("ATTACHMENT"), "Downloading attachment: ", attachment.url);
           await fetch(attachment.createFileURL())
             .then(async (response) => Buffer.from(await response.arrayBuffer()))
             .then((buffer) => {
-              if (!buffer) throw new Error("[ERROR] Buffer is undefined");
+              if (!buffer) throw new Error(color.bgRed("ERROR"), "Buffer is undefined");
+              if (attachment.contentType === "text/plain") throw new Error(color.bgRed("ERROR"), "This content type is not supported");
               const aiObject = fileToGenerativePart(Buffer.from(buffer), attachment.contentType)
               imageParts.push(aiObject)
             }).finally(async () => {
               if (imageParts.length > 4194304) {
-                throw new Error("[ERROR] Attachment is too large...");
+                throw new Error(color.bgRed("ERROR"), "Attachment is too large...");
               } else {
-                console.log("[ATTACHMENT] Attachment has been downloaded: ", attachment.url);
+                console.log(colors.bgWhite("ATTACHMENT"), "Attachment has been downloaded: ", attachment.url);
                 chatHistroy.push({ role: "user", parts: [msg.content, imageParts] })
                 const result = await image_model.generateContent([msg.content, ...imageParts]);
-                console.log("[GEMINI_API] Gemini has responsed: ", result.response.text())
+                console.log(colors.bgMagenta("GEMINI_API"), "Gemini has responsed: ", result.response.text())
                 msg.reply(result.response.text());
                 chatHistroy.push({ role: "model", parts: result.response.text() });
-                console.log("[CHAT_HISTORY] Chat history has been updated: ", chatHistroy)
+                console.log(colors.bgCyan("CHAT"), "Chat history has been updated: ", chatHistroy)
               }
             })
         }
       } else {
-        console.log("[MESSAGE] New message: ", msg.content);
-        console.log("[GEMINI_API] Using: ", text_model.model.toString());
+        console.log(colors.bgWhite("MESSAGE"), "New message: ", msg.content);
+        console.log(colors.bgMagenta("GEMINI_API"), "Using: ", text_model.model.toString());
         chatHistroy.push({ role: "user", parts: msg.content })
         const result = await text_chat.sendMessage(msg.content);
-        console.log("[GEMINI_API] Gemini has responsed: ", result.response.text())
+        console.log(colors.bgMagenta("GEMINI_API"), "Gemini has responsed: ", result.response.text())
         await msg.reply(result.response.text());
         chatHistroy.push({ role: "model", parts: result.response.text() })
-        console.log("[CHAT_HISTORY] Chat history has been updated: ", chatHistroy)
+        console.log(colors.bgCyan("CHAT"), " Chat history has been updated: ", chatHistroy)
       }
     }
   } catch (e) {
-    throw new Error("[ERROR]", e);
+    console.error(colors.bgRed("ERROR"), e);
   }
 });
 
 if (!process.env.RVLT_TOKEN, process.env.RVLT_TOKEN === "") {
-  throw new Error("[ERROR] Missing Revolt token");
+  throw new Error(color.bgRed("ERROR"), "Missing Revolt token");
 } else {
   client.loginBot(process.env.RVLT_TOKEN);
 }
